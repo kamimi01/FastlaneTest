31a79f9d402860271b7ece8e4e57424b9265b248
mika.urakawa <awakaru0122@gmail.com>
2023-06-11 13:42:00 +0900

    feat: create release note with swift

--- fastlane/release.swift
+++ fastlane/release.swift
@@ -27,8 +27,35 @@ precondition(envArgs["TAG"] != nil)
 let tag = envArgs["TAG"]!
 print(tag)
 
-try safeShell("cd .. && git tag \(tag)")
+try safeShell("cd .. && git tag \(tag) && git push origin \(tag)")
 let result = try safeShell("git log --oneline")
 print(result)
 
 // 2. リリースノートを作成する
+let url = URL(string: "https://api.github.com/repos/kamimi01/FastlaneTest/releases")!
+let headers = [
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer \(envArgs["GITHUB_TOKEN"]!)",
+    "X-GitHub-Api-Version": "2022-11-28"
+]
+
+let data: [String: Any] = [
+    "tag_name": tag, 
+    "name": tag,
+    "body": tag,
+    "prerelease": true
+]
+let httpBody = try! JSONSerialization.data(withJSONObject: data, options: [])
+
+var request = URLRequest(url: url)
+request.httpMethod = "POST"
+request.allHTTPHeaderFields = headers
+request.httpBody = httpBody
+
+do {
+    let (data, urlResponse) = try await URLSession.shared.data(for: request)
+    let response = urlResponse as! HTTPURLResponse
+    print(response)
+} catch {
+    print(error)
+}
