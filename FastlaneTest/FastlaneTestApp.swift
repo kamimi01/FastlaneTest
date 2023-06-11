86700c00ba1a025311f85de9dccfb6d696b9105f
mika.urakawa <awakaru0122@gmail.com>
2023-06-11 11:18:23 +0900

    feat: add fastlane

---
+++ FastlaneTest/FastlaneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneTestApp.swift
+//  FastlaneTest
+//
+//  Created by mikaurakawa on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fastlane/hello.swift
@@ -0,0 +1,4 @@
+#!/usr/bin/env swift
+
+let locationName: String = "六本木"
+print("ようこそ" + locationName + "へ！")
